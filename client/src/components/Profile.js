5ee38b09e609f0746183ca21bf8c0896c76f572e
Trappin Recidivism <austinslater58@gmail.com>
2023-03-21 11:38:43 -0500

    wrote a basic Profile component to import to App.js

---
+++ client/src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Here is where the user's profile information would go.</p>
+    </div>
+  );
+}
+
+export default Profile;
